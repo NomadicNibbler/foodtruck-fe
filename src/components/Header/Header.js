1d26988d57b914e79326e5fc49c948d5de9eb47a
Elizabeth Hahn <elizhahn87@gmail.com>
2021-05-14 13:19:33 -0600

    Create Header component, add normalize.css file

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="app-title">The Nomadic Nibbler</h1>
+    </header>
+  )
+}
+
+export default Header
