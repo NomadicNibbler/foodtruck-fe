b629a397fee69862cc62c6aaf27843fe9b12e094
Elizabeth Hahn <elizhahn87@gmail.com>
2021-05-14 16:17:59 -0600

    Add styling to Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-// import './App.css';
 import Header from './Header/Header'
 
 function App() {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Header = () => {
   return (
+    <>
     <header className="header">
       <h1 className="header-title">The Nomadic Nibbler</h1>
     </header>
+    <div className="header-color-bar"></div>
+    </>
   )
 }
 
