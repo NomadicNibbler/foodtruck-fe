33bd7a1cc1d09caf85d4acbc18c9ab70565c49a2
Elizabeth Hahn <elizhahn87@gmail.com>
2021-05-14 17:50:38 -0600

    Add truck animation to header and responsive styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { ReactComponent as FoodTruck } from '../../assets/food-truck.svg';
 
 const Header = () => {
   return (
     <>
     <header className="header">
       <h1 className="header-title">The Nomadic Nibbler</h1>
+      <FoodTruck className="food-truck-img"/>
+      <div class="speed-line"></div>
+      <div class="speed-line second"></div>
+      <div class="shadow"></div>
     </header>
     <div className="header-color-bar"></div>
     </>
