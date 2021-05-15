ebd7b3fe6e1aa9c10ccd1dde18bdcd577a6e987f
Joel <wilddroppings@gmail.com>
2021-05-15 13:25:55 -0600

    Flesh out inputs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -28,11 +28,42 @@ class Form extends Component {
     render() {
         return (
             <form>
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
+                <input
+                    name='first'
+                    placeholder='First Name'
+                    type='text'
+                    value={this.state.first}
+                    onChange={this.handleChange}
+                 />
+                <input 
+                    name='last'
+                    placeholder='Last Name'
+                    type='text'
+                    value={this.state.last}
+                    onChange={this.handleChange}
+
+                />
+                <input
+                    name='address'
+                    placeholder='Address'
+                    type='text'
+                    value={this.state.address}
+                    onChange={this.handleChange}
+                 />
+                <input 
+                    name='city'
+                    placeholder='City'
+                    type='text'
+                    value={this.state.city}
+                    onChange={this.handleChange}
+                />
+                <input 
+                    name='zip'
+                    placeholder='Zip Code'
+                    type='text'
+                    value={this.state.zip}
+                    onChange={this.handleChange}
+                />
                 <button>Login</button>
                 <button>New User?</button>
                 <button>Let's Eat!</button>
