b3c18cca1569181a5b0419c7f24125a9194d185e
Joel <wilddroppings@gmail.com>
2021-05-15 13:06:41 -0600

    Add basic state and methods to form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,13 +4,33 @@ class Form extends Component {
     constructor() {
         super()
         this.state = {
-
+            first: '',
+            last: '',
+            address: '',
+            city: '',
+            zip: ''
         }
     }
 
+    handleSubmit = () => {
+
+    }
+
+    handleChange = (e) => {
+
+    }
+
+    clearInputs = () => {
+
+    }
+
     render() {
-        return null
+        return (
+            <form>
+                <input
+            </form>
+        )
     }
 }
 
-export default Form
+export default Form;
