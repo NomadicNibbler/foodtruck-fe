7bc45a8351475f5775fe399a2bb9ba39174fafda
Elizabeth Hahn <elizhahn87@gmail.com>
2021-05-15 15:55:21 -0600

    Create trucks mock data file

---
+++ src/trucks.js
@@ -0,0 +1,18 @@
+const trucks = [
+  {
+    "id": "1",
+    "lat": "49.246292",
+    "long": "-123.116827",
+    "name": "truck_1",
+    "distance": "2.4",
+    "logo_small": "https:\/\/cdn.streetfoodapp.com\/images\/bc-taco\/logo\/90w_square.png"
+  },
+  {
+    "id": "2",
+    "lat": "49.246292",
+    "long": "-123.116527",
+    "name": "truck_2",
+    "distance": "1.2",
+    "logo_small": "https:\/\/cdn.streetfoodapp.com\/images\/beavertails\/logo\/1.90w.png"
+  }
+]
