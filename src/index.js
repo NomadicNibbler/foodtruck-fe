8b80246bd19c089406dedc1385868681aa635ac5
Joel <wilddroppings@gmail.com>
2021-05-16 14:29:56 -0600

    Removed redundant browser router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
