5380a4742d58f42ea584e6b353c0083e7f70c246
Elizabeth Hahn <elizhahn87@gmail.com>
2021-05-16 12:21:27 -0600

    Add styling to MapView component

--- src/components/MapView/MapView.js
+++ src/components/MapView/MapView.js
@@ -1,15 +1,9 @@
 import  React from 'react';
-import { Component } from 'react';
+// import { Component } from 'react'; //may need to hold state here for select menu
 import { GoogleMap, LoadScript, MarkerClusterer, Marker } from '@react-google-maps/api';
 import { Link } from 'react-router-dom';
 import truckIcon from '../../assets/food-truck.svg';
 const apiKey = process.env.REACT_APP_API_KEY;
-
-const containerStyle = {
-  height: '400px',
-  width: '100%',
-
-}
 
 function createKey(location) {
   return location.lat + location.lng
@@ -17,47 +11,47 @@ function createKey(location) {
 
 const MapView = ({ truckList, center }) => {
 return (
-    <main className="map-container">
-      <Link to="/trucklist">
-        <button>Toggle</button>
-      </Link>
-      <select>
-        <option>please select</option>
-        <option>cash only</option>
-        <option>closests</option>
-        <option>farthest</option>
-      </select>
-      <button>change location</button>
-      <LoadScript
-        googleMapsApiKey={apiKey}
-      >
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={13}
+    <main>
+      <div className="map-buttons-container">
+        <Link to="/trucklist">
+          <button className="button">Truck List</button>
+        </Link>
+        <Link to="/userform">
+          <button className="button">Change Location</button>
+        </Link>
+      </div>
+      <section className="map-container">
+        <LoadScript
+          googleMapsApiKey={apiKey}
         >
-          <>
-          <Marker
-            position={center}
-          />
-          <MarkerClusterer>
-              {(clusterer) =>
-              truckList.map((location) => (
-                <Marker 
-                  key={createKey(location)}
-                  position={location} 
-                  clusterer={clusterer}
-                  icon={{
-                    url: truckIcon,
-                    scaledSize: new window.google.maps.Size(40, 40)
-                  }} 
-                />
-              ))
-            }
-          </MarkerClusterer>
-          </>
-        </GoogleMap>
-      </LoadScript>
+          <GoogleMap
+            mapContainerClassName="map"
+            center={center}
+            zoom={13}
+          >
+            <>
+            <Marker
+              position={center}
+            />
+            <MarkerClusterer>
+                {(clusterer) =>
+                truckList.map((location) => (
+                  <Marker 
+                    key={createKey(location)}
+                    position={location} 
+                    clusterer={clusterer}
+                    icon={{
+                      url: truckIcon,
+                      scaledSize: new window.google.maps.Size(40, 40)
+                    }} 
+                  />
+                ))
+              }
+            </MarkerClusterer>
+            </>
+          </GoogleMap>
+        </LoadScript>
+      </section>
     </main>
   )
 
