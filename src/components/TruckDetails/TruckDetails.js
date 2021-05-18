22b0d1538f8ee86d6442012be1698de0085c62b2
Elizabeth Hahn <elizhahn87@gmail.com>
2021-05-17 21:12:41 -0600

    Adjust styling of truck details

--- src/components/TruckDetails/TruckDetails.js
+++ src/components/TruckDetails/TruckDetails.js
@@ -7,8 +7,13 @@ const TruckDetails = () => {
     <section className="truck-details-container">
       <article className="truck-logo-container">
         <img src={truck.data.attributes.logo} alt={`${truck.data.name} logo`} className="truck-logo"/>
-        <i class="truck-social-icon"></i>
+        <div className="truck-links">
+          <h1 className="truck-name">{truck.data.attributes.name}</h1>
+          <a href={truck.data.attributes.website} className="truck-website">{truck.data.attributes.website}</a>
+        </div>
       </article>
+      <p>Come visit us!</p>
+      <p className="truck-description-intro">About Us:</p>
       <p className="truck-description">{truck.data.attributes.description}</p>
     </section>
   )
