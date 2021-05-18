2d54fe923a15ded3200c0137fb996bbd863e882c
Elizabeth Hahn <elizhahn87@gmail.com>
2021-05-17 21:56:11 -0600

    Dynamically render social media icons with functioning links

--- src/components/SocialMediaLinks/SocialMediaLinks.js
+++ src/components/SocialMediaLinks/SocialMediaLinks.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { SiFacebook } from 'react-icons/si';
+import { SiTwitter } from 'react-icons/si';
+import { SiInstagram } from 'react-icons/si';
 
 const SocialMediaLinks = ({ links }) => {
-  console.log(links)
+  
+    const linkList = links.map(link => {
+      const socialMediaOutlet = Object.keys(link);
+      switch (true) {
+        case socialMediaOutlet[0] === "facebook" :
+          return  <a href= {`https://facebook.com/${Object.values(link)}`}>
+                  <li>
+                    <SiFacebook className="social-media-icon"/>
+                    </li>
+                  </a>
+                
+        case socialMediaOutlet[0] === "twitter" :
+          return  <a href= {`https://twitter.com/${Object.values(link)}`}>
+                  <li>
+                    <SiTwitter className="social-media-icon"/>
+                  </li>
+                  </a>
+                
+
+        case socialMediaOutlet[0] === "instagram" :
+          return  <a href= {`https://instagram.com/${Object.values(link)}`}>
+                  <li>
+                    <SiInstagram className="social-media-icon"/>
+                    </li>
+                  </a>
+              
+        default :
+          return <p>Come visit us today!!</p>
+      }
+    })
+  
   return (
-    <ul>
+    <ul className="social-link-list">
+      {linkList}
     </ul>
   )
 }
--- src/mocktruck.js
+++ src/mocktruck.js
@@ -1,7 +1,7 @@
 const truck = {
   "data": {
     "type": "trucks",
-    "id": "1",
+    // "id": "1",
     "attributes": {
       "name": "arturosmexico2go",
       "payment_methods": [ 
@@ -13,7 +13,7 @@ const truck = {
       "website": "arturos2go.com",
       "socials": [{"facebook": "arturosmexico2go"}, {"twitter":"arturomexico2go"}, {"instagram": "arturos2go"}],
       "phone": "(604) 202-4043",
-      "email": "info@arturos2go.com",
+      // "email": "info@arturos2go.com",
       "description": "Arturo's unique recipes are a fusion of Spanish and traditional Mexican. Clean, simple and healthy Mexican food. Only 3 people prepare the food we serve to our clients, from the local produce and local butcher, there is not third parties when it comes to prepare our dishes. We closely follow Health Authority guidances and protocols to operate our business. We have been serving take out food at open spaces since 2010, and we will continue doing it, safety is our priority.",
       "logo": "https://cdn.streetfoodapp.com/images/arturos-to-go/logo/1.90w@2x.png",
       // "logo": "https://cdn.streetfoodapp.com/images/bc-taco/logo/90w_square@2x.png",
