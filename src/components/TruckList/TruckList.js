658aa4cef09d6a6fcdcee6989ec93566c5d50c66
Joel <wilddroppings@gmail.com>
2021-05-20 22:33:21 -0600

    button layout

--- src/components/TruckList/TruckList.js
+++ src/components/TruckList/TruckList.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
 import TruckCard from '../TruckCard/TruckCard';
 
 const TruckList = ({ truckList }) => {
     return (
         <>
-            <button className='button'>Truck Map</button>
-            <button className='button'>Filter</button>
+            <div className='button-container'>
+                <Link to='/map'>
+                    <button className='button'>Truck Map</button>
+                </Link>
+                <button className='button'>Filter</button>
+            </div>
             {truckList.map(truck => {
                 return <TruckCard
                         key={truck.attributes.name}
