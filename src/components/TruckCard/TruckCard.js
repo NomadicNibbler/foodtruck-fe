5bf05f41e682c4871a246a7df52591872ec58dff
Joel <wilddroppings@gmail.com>
2021-05-20 22:13:31 -0600

    Add style for truck cards

--- src/components/TruckCard/TruckCard.js
+++ src/components/TruckCard/TruckCard.js
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom';
 const TruckCard = ({ logo, distance, name, description}) => {
     return (
         <Link to={`/trucks/${name}`} className='truck-card' id={name}>
-            <h2>{name}</h2>
-            {logo && <img src={logo} alt='truck logo' className='truck-logo'/>}
-            <h2>{`${distance} miles`}</h2>
-            <h2>{description}</h2>
+            <div className='box1'>
+                <h2 className='truck-name'>{name}</h2>
+                <h2>{`${distance} miles`}</h2>
+            </div>
+            <div className='box2'>
+                {logo && <img src={logo} alt='truck logo' className='truck-logo'/>}
+                <h2>{description}</h2>
+            </div>
         </Link>
     )
 }
