52b0e9eaec7e741f2871aeaf78a9a08b3ab56ea5
Elizabeth Hahn <elizhahn87@gmail.com>
2021-05-25 21:17:04 -0600

    Implemented a new version of the service worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,8 @@
-const cacheName = 'v1';
-// const self = this;
+// const cacheName = 'v1';
+
+// the cache version gets updated every time there is a new deployment
+const CACHE_VERSION = 10;
+const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 const urlsToCache = [
   '/site.webmanifest',
@@ -17,53 +20,149 @@ const urlsToCache = [
   '/map',
 ];
 
-// Install a service worker
-self.addEventListener('install', event => {
-  //perform install steps
-  self.skipWaiting()
-  event.waitUntil(
-    caches.open(cacheName)
-    .then(cache => {
-      console.log('Opened cache')
-      return cache.addAll(urlsToCache)
-    })
-  )
-})
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Activated');
-  event.waitUntil(
-    caches.keys().then(cacheName => {
+// on activation we clean up the previously registered service workers
+self.addEventListener('activate', evt =>
+  evt.waitUntil(
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheName.map(cache => {
-          if (cache !== cacheName) {
-            console.log('Service Worker: clearing old cache');
-            return caches.delete(cache);
+        cacheNames.map(cacheName => {
+          if (cacheName !== CURRENT_CACHE) {
+            return caches.delete(cacheName);
           }
         })
       );
     })
   )
-})
+);
 
+// on install we download the routes we want to cache for offline
+self.addEventListener('install', evt =>
+  evt.waitUntil(
+    caches.open(CURRENT_CACHE).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  )
+);
 
-self.addEventListener('fetch', event => {
-  // if (!(event.request.url.indexOf('http') === 0)) return;
-  // if(event.request.method === 'POST') return; 
-  event.respondWith(
-    caches.open(cacheName).then(cache => {
-      return cache.match(event.request).then(response => {
-        return (
-          response ||
-          fetch(event.request).then(response => {
-            cache.put(event.request, response.clone());
-            return response;
-          })
-        );
-      });
-    }),
+// fetch the resource from the network
+const fromNetwork = (request, timeout) =>
+  new Promise((fulfill, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(response => {
+      clearTimeout(timeoutId);
+      fulfill(response);
+      update(request);
+    }, reject);
+  });
+
+// fetch the resource from the browser cache
+const fromCache = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      cache
+        .match(request)
+        .then(matching => matching || cache.match('/offline/'))
+    );
+
+// cache the current page to make it available for offline
+const update = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      fetch(request).then(response => cache.put(request, response))
+    );
+
+// general strategy when making a request (eg if online try to fetch it
+// from the network with a timeout, if something fails serve from cache)
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    fromNetwork(evt.request, 10000).catch(() => fromCache(evt.request))
   );
+  evt.waitUntil(update(evt.request));
 });
+
+// Install a service worker
+// self.addEventListener('install', event => {
+//   //perform install steps
+//   self.skipWaiting()
+//   event.waitUntil(
+//     caches.open(cacheName)
+//     .then(cache => {
+//       console.log('Opened cache')
+//       return cache.addAll(urlsToCache)
+//     })
+//   )
+// })
+
+// self.addEventListener('activate', (event) => {
+//   console.log('Service Worker: Activated');
+//   event.waitUntil(
+//     caches.keys().then(cacheName => {
+//       return Promise.all(
+//         cacheName.map(cache => {
+//           if (cache !== cacheName) {
+//             console.log('Service Worker: clearing old cache');
+//             return caches.delete(cache);
+//           }
+//         })
+//       );
+//     })
+//   )
+// })
+
+// self.addEventListener('fetch', (event) => {
+//   console.log('fetching from network');
+//   event.respondWith(
+//     fetch(event.request)
+//       .then(res => {
+//         const responseClone = response.clone();
+//         caches
+//         .open(cacheName)
+//         .then(cache => {
+//           cache.put(event.request, resClone);
+//         });
+//         return res;
+//       }).catch(err => caches.match(event.request).then(res => res))
+//   );
+// });
+
+
+//activate version 2
+// self.addEventListener('activate', (event) => {
+//   console.log('Service Worker: Activated');
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+      
+//         cacheNames.map(cache => {
+//           if (cache.indexOf(cacheName) < 0) {
+//             console.log('Service Worker: clearing old cache');
+//             return caches.delete(cacheName);
+//           }
+//         })
+//     })
+//   )
+// })
+
+
+// self.addEventListener('fetch', event => {
+//   // if (!(event.request.url.indexOf('http') === 0)) return;
+//   // if(event.request.method === 'POST') return; 
+//   event.respondWith(
+//     caches.open(cacheName).then(cache => {
+//       return cache.match(event.request).then(response => {
+//         return (
+//           response ||
+//           fetch(event.request).then(response => {
+//             cache.put(event.request, response.clone());
+//             return response;
+//           })
+//         );
+//       });
+//     }),
+//   );
+// });
 
 
 
@@ -185,7 +284,7 @@ self.addEventListener('fetch', event => {
 // }
 
 // self.addEventListener('fetch', evt => {
-//   evt.respondWith(fromNetwork(evt.request, 10000).catch(function () {
+//   evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
 //     return fromCache(evt.request);
 //   }));
 // });
@@ -303,73 +402,4 @@ self.addEventListener('fetch', event => {
 //   );
 // });
 
-//TWO
 
-// the cache version gets updated every time there is a new deployment
-// const CACHE_VERSION = 10;
-// const CURRENT_CACHE = `main-${CACHE_VERSION}`;
-
-// // these are the routes we are going to cache for offline support
-// const cacheFiles = ['/', '/about-me/', '/projects/', '/offline/'];
-
-// // on activation we clean up the previously registered service workers
-// self.addEventListener('activate', evt =>
-//   evt.waitUntil(
-//     caches.keys().then(cacheNames => {
-//       return Promise.all(
-//         cacheNames.map(cacheName => {
-//           if (cacheName !== CURRENT_CACHE) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   )
-// );
-
-// // on install we download the routes we want to cache for offline
-// self.addEventListener('install', evt =>
-//   evt.waitUntil(
-//     caches.open(CURRENT_CACHE).then(cache => {
-//       return cache.addAll(cacheFiles);
-//     })
-//   )
-// );
-
-// // fetch the resource from the network
-// const fromNetwork = (request, timeout) =>
-//   new Promise((fulfill, reject) => {
-//     const timeoutId = setTimeout(reject, timeout);
-//     fetch(request).then(response => {
-//       clearTimeout(timeoutId);
-//       fulfill(response);
-//       update(request);
-//     }, reject);
-//   });
-
-// // fetch the resource from the browser cache
-// const fromCache = request =>
-//   caches
-//     .open(CURRENT_CACHE)
-//     .then(cache =>
-//       cache
-//         .match(request)
-//         .then(matching => matching || cache.match('/offline/'))
-//     );
-
-// // cache the current page to make it available for offline
-// const update = request =>
-//   caches
-//     .open(CURRENT_CACHE)
-//     .then(cache =>
-//       fetch(request).then(response => cache.put(request, response))
-//     );
-
-// // general strategy when making a request (eg if online try to fetch it
-// // from the network with a timeout, if something fails serve from cache)
-// self.addEventListener('fetch', evt => {
-//   evt.respondWith(
-//     fromNetwork(evt.request, 10000).catch(() => fromCache(evt.request))
-//   );
-//   evt.waitUntil(update(evt.request));
-// });
